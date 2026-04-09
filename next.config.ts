59ebd9f924a122cad680a7e2aae38d7c585e646d
aryamandbest <aryamandbest@gmail.com>
2026-04-09 17:13:34 +0000

    fix 12

--- next.config.ts
+++ next.config.ts
@@ -1,34 +1,13 @@
-import type {NextConfig} from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  output: "export",
+  distDir: ".next",
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-    ],
+    unoptimized: true,
+  },
+  env: {
+    GEMINI_API_KEY: "AIzaSyBD3QycLZuyJA500YKXYyYeUcuv7x6P6Lw",
   },
 };
 
